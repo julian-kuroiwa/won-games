aef99121660eac9e04e2bbef350b2e1aca3d9bf7
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-09-09 07:59:36 -0300

    Logo: creating tests with TDD

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -11,14 +11,28 @@ describe('<Logo />', () => {
       color: '#FAFAFA',
     })
   })
-})
 
-describe('<Logo />', () => {
   it('should render a black label by default', () => {
     renderWithTheme(<Logo color="black" />)
 
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517',
+    })
+  })
+
+  it('should render a normal size logo', () => {
+    renderWithTheme(<Logo />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem',
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem',
     })
   })
 })
