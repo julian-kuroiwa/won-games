89578ef241e959098a4b0cf8c7b4d49ee5079857
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-10-05 08:38:33 -0300

    Heading: size and lineColor properties

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -21,7 +21,7 @@ describe('<Heading />', () => {
   it('should render a heading with a line to the left side', () => {
     renderWithTheme(<Heading lineLeft>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
-      'border-left': '0.7rem solid #3CD3C1',
+      'border-left': '0.7rem solid #F231A5',
     })
   })
 
@@ -29,10 +29,43 @@ describe('<Heading />', () => {
     renderWithTheme(<Heading lineBottom>Won Games</Heading>)
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
       'border-bottom',
-      '0.4rem solid #F231A5',
+      '0.5rem solid #F231A5',
       {
         modifier: '::after',
       }
     )
+  })
+
+  it('should render a heading with a small size', () => {
+    renderWithTheme(
+      <Heading size="small" lineBottom>
+        Won Games
+      </Heading>
+    )
+    expect(
+      screen.getByRole('heading', { name: /won games/i })
+    ).toHaveStyleRule({ fontSize: '1.6rem' })
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after',
+      }
+    )
+  })
+
+  it('should render a Heading with a secondary line color', () => {
+    renderWithTheme(
+      <Heading lineColor="secondary" lineLeft lineBottom>
+        Lorem Ipsum
+      </Heading>
+    )
+
+    const heading = screen.getByRole('heading', { name: /lorem ipsum/i })
+    expect(heading).toHaveStyle({ 'border-left': '0.7rem solid #3CD3C1' })
+    expect(heading).toHaveStyleRule('border-bottom', '0.5rem solid #3CD3C1', {
+      modifier: '::after',
+    })
   })
 })
