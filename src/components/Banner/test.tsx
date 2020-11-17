a17446e69ecf23d4c7d911fd058b72760b3649ea
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-11-17 15:07:49 -0300

    Banner: create tests

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -4,24 +4,23 @@ import { renderWithTheme } from '../../utils/tests/helpers'
 import Banner from '.'
 
 describe('<Banner />', () => {
-  it('should render correctly', () => {
-    const { container } = renderWithTheme(
-      <Banner
-        title="Test"
-        subtitle="Sub test"
-        img="https://source.unsplash.com/user/willianjusten/1042x580"
-        buttonLabel="Click here"
-        buttonLink="/link"
-      />
-    )
+  const props = {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season</p>',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+  }
 
-    expect(screen.getByTitle(/test/i)).toBeInTheDocument()
-    expect(screen.getByTitle(/Sub test/i)).toBeInTheDocument()
+  it('should render correctly', () => {
+    renderWithTheme(<Banner {...props} />)
+
     expect(
-      screen.GetByRole('img', {
-        src: /https:\/\/source.unsplash.com\/user\/willianjusten\/1042x580/i,
-      })
+      screen.getByRole('heading', { name: /defy death/i })
     ).toBeInTheDocument()
-    //imagem exista
+    expect(
+      screen.getByRole('heading', { name: /Play the new CrashLands season/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /defy death/i })).toBeInTheDocument()
   })
 })
